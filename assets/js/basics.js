ff1bb2b4289355873f0af9a211389c9be5aae2ba
josy Santos <josydossantos2002@gmail.com>
2023-04-05 23:48:06 +0100

    basic functionalities

--- assets/js/basics.js
+++ assets/js/basics.js
@@ -1,18 +1,20 @@
 const buttonsToAddNumbers = document.querySelectorAll(".is-numerics-elements button"),
       buttonsToAddOperacional = document.querySelectorAll(".is-operational-elements button"),
-    input = document.getElementById("theInputArea")
+    input = document.getElementById("theInputArea");
 
 const btnClearAll = document.querySelector("#clearAll"), 
 btnClearSect = document.querySelector("#ClearSect"), 
 btnClearLast = document.querySelector("#clearLast"),
-btnPercent = document.querySelector("#percent")   
+btnPercent = document.querySelector("#percent");   
 
 const btnHistory = document.querySelector("footer button"),
-btnResult = document.querySelector("footer button:nth-child(2)")
+btnResult = document.querySelector("footer button:nth-child(2)");
 
 let listBtnToAddNmbers = buttonsToAddNumbers.length, listBtnToAddOperacional = buttonsToAddOperacional.length,
  verification_1 = (listBtnToAddNmbers && listBtnToAddOperacional) > 0, 
- btnEspecialFunction = 9, elements
+ btnEspecialFunction = 9, elements;
+
+const allOperacionalChars = ["+", "-", "*", "/", "%"]
 
 if(verification_1){
     for(elements = 0; elements < listBtnToAddNmbers; elements++){
@@ -46,6 +48,7 @@ if(verification_1){
 btnClearAll.onclick = () =>{
     input.value = "0"
 }
+
 btnClearLast.onclick = () =>{
     let newInputValue = Array.from(input.value)
     elements = newInputValue.length
@@ -56,13 +59,26 @@ btnClearLast.onclick = () =>{
 }
 btnClearSect.onclick = () =>{
     let String = input.value
-    let BreakString = String.split(/\W/)
-    let charRecovery = String.split(/\w/)
+    let allComposition = String.split(/\b/)
+    let lastItem = allComposition.length - 1
     input.value = ""
+    for(let interations = 0; interations < lastItem; interations++)
+    {
+        input.value += allComposition[interations]
+    }
 }
 
 btnPercent.onclick = () =>{
-    //verify if has more zero than operacional, if true put and make it after else give result rigth now.
+    if(input.value != 0){
+        let findResult = input.value.match(/\W/)
+        if(findResult < 1 || findResult === null)
+            return input.value = parseInt(input.value) / 100
+        return input.value += "%" 
+    }
+}
+
+btnResult.onclick = () =>{
+    FinalResult()
 }
 
 
@@ -73,9 +89,11 @@ let AddCharsInInput = (element) => {
 }
 
 let FinalResult = () =>{
-    //calc the final result
+    let result = eval(input.value)
+    input.value = result
 }
 
 let exceptionsError = () =>{
     //make the exceptions and give basic errors with typescript
 }
+
